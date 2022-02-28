2a30c5b9c33de71c17919734c6bab004642edb39
Robel Schwarz <itsrobel.schwarz@gmail.com>
2022-02-28 17:46:10 -0500

    stuff 2

--- RPS.py
+++ RPS.py
@@ -2,63 +2,51 @@ from cgi import print_directory
 import random
 from collections import Counter
 import pandas as pd
-
+from RPS_game import abbey
+from itertools import permutations
+from itertools import product
+import operator
 # The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
 rps_index = {"R": "P", "P": "S", "S": "R"}
 
 
 def player(prev_play, opponent_history=[]):
+    dict_rps= get_perm("RPS", 3)
+    df_play=pd.DataFrame.from_dict(dict_rps,  orient='index',  columns=['value'])
+    df_play.index.name = "key"
+
+
+    if not prev_play:
+        prev_play = 'R'
+    
     opponent_history.append(prev_play)
+    # print(df_play.index.name)
+    opponent_hist_str = "".join(opponent_history)
 
-    play_order = [{
-        "RR": 0,
-        "RP": 0,
-        "RS": 0,
-        "PR": 0,
-        "PP": 0,
-        "PS": 0,
-        "SR": 0,
-        "SP": 0,
-        "SS": 0,
-    }]
-    df = pd.DataFrame({"History": opponent_history})
-    # print(df.value_counts().max())
-    df = df.value_counts()
-    # print(df.idxmax()[0])
+    find_and_sort(df_play, opponent_hist_str)
+    # print(df_play.sort_values(by="value",ascending=False))
+    # print(df_play)
+    # df_play[]
+    # for i in df_play:
+    #     df_play["value"] = ohs.count(df_play["key"])
+    # print(df_play)
+    # print(max(df_play, key=operator.itemgetter(1)))
+    return abbey(prev_opponent_play=prev_play)
 
-    # df1 = pd.DataFrame(data=df['History'].value_counts(),
-    #                    columns=[['History', 'Count']])
-    # guess = random.choice(['R', 'P', 'S'])
-    guess = "R"
-    style = random.randint(0, 1)
-    if len(opponent_history) > 3:
-        if style == 1:
-            guess = opponent_history[-1]
-            guess = rps_index[guess]
-        if style == 0:
+def get_perm(str1, rno):
+  chars = list(str1)
+  results = {}
+  for c in product(chars, repeat = rno):
+    results["".join(list(c))] = 0
+  return results
 
-            guess = opponent_history[-1]
-            guess = rps_index[guess]
+def find_and_sort(df, string):
+    for index, row in df.iterrows():
+        # print(index)
+        # print(string.count(index))
+        # print(row['value'])
+        row['value'] = string.count(index)
 
-            last_two = "".join(opponent_history[-2:])
-            if len(last_two) == 2:
-                play_order[0][last_two] += 1
+    df = (df.sort_values(by="value",ascending=False))
 
-            potential_plays = [
-                prev_play + "R",
-                prev_play + "P",
-                prev_play + "S",
-            ]
-    # print(potential_plays)
-            sub_order = {
-                k: play_order[0][k]
-                for k in potential_plays if k in play_order[0]
-            }
-
-            prediction = max(sub_order, key=sub_order.get)[-1:]
-            guess = prediction
-    # print(opponent_history[-2:])
-    # print(guess)
-    # print(opponent_history)
-
-    return guess
+    return df
--- RPS_game.py
+++ RPS_game.py
@@ -2,8 +2,6 @@
 
 import random
 
-from numpy import percentile
-
 
 def play(player1, player2, num_games, verbose=False):
     p1_prev_play = ""
@@ -100,14 +98,13 @@ def abbey(prev_opponent_play,
         prev_opponent_play + "P",
         prev_opponent_play + "S",
     ]
-    # print(potential_plays)
+
     sub_order = {
         k: play_order[0][k]
         for k in potential_plays if k in play_order[0]
     }
 
     prediction = max(sub_order, key=sub_order.get)[-1:]
-    # print(prediction)
 
     ideal_response = {'P': 'S', 'R': 'P', 'S': 'R'}
     return ideal_response[prediction]
