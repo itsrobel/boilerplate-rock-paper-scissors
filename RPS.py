f0b14f560a44f95a2d10a0b0402a8a081a4c39c1
Robel Schwarz <amadeus@Robels-MacBook-Pro.local>
2022-03-02 14:43:05 -0500

    clean

--- RPS.py
+++ RPS.py
@@ -1,17 +1,7 @@
-from cgi import print_directory
-import random
-from collections import Counter
-from matplotlib.pyplot import axes, axis
 import pandas as pd
-from RPS_game import abbey, mrugesh
-from itertools import permutations
 from itertools import product
-import operator
 
 from beatAbbey import beatAbbey
-# The example
-
-# unction below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
 rps_index = {"R": "P", "P": "S", "S": "R"}
 
 
@@ -26,17 +16,12 @@ def player(prev_play, opponent_history=[]):
     next_play = select_rank(df_new, opponent_history)
     if len(opponent_history) < 2:
         next_play = "P"
-        # print(opponent_history)
     abbey = beatAbbey(prev_play)
 
     if len(opponent_history) > 1000 & len(opponent_history) < 2000:
         return abbey
     else:
         return rps_index[next_play]
-    # elif rn == 3 | rn == 2:
-    #     return abbey(prev_play)
-    # elif rn < 4:
-    #     return mrugesh(prev_opponent_play=prev_play)
 
 
 def get_perm(str1, rno):
@@ -66,9 +51,5 @@ def select_rank(df, hist):
     df['play'] = df.apply(lambda row: find(row["play"]), axis=1)
     dist = (df[df["play"].notnull()])
     play = dist['value'].idxmax()
-
-    # print(df)
-    # print(hist)
     play = dist.loc[play]['play'][-1:]
-    # print(play)
     return play
